edc9696e8933e8d65fa499557b4da1b0ae101922
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2019-10-23 21:04:23 +0200

    Improve utils

--- util.h
+++ util.h
@@ -1,12 +1,14 @@
 #ifndef MASTER_PROJECT_UTIL_H
 #define MASTER_PROJECT_UTIL_H
 
-void byteArraysXOR(uint8_t *out, uint8_t *array1, uint8_t *array2, int len1, int len2);
+#include <stdint.h>
+
+void byteArraysXOR(uint8_t *out, const uint8_t *array1, const uint8_t *array2, int len1, int len2);
 
 void itoa(int value, char* str, int base);
 
 int compareBinaryStrings(char *string1, char *string2);
 
-void generateRandomBytes(uint8_t *bin, int binSize);
+void generateRandomBytes(uint8_t *bin, unsigned int binSize);
 
 #endif //MASTER_PROJECT_UTIL_H
