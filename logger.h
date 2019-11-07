e93bfb429b65d60f4c1befbaab05bfb4718215e3
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2019-11-07 13:45:52 +0100

    Disable logging if zf_log not available

--- logger.h
+++ logger.h
@@ -1,6 +1,7 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#ifdef HAVE_ZFLOG
 #include "zf_log/zf_log/zf_log.h"
 
 #define LOGGER_DEBUG ZF_LOG_DEBUG
@@ -9,5 +10,13 @@
 #define LOGGER_ERROR ZF_LOG_ERROR
 
 #define logger_log(lvl, ...) ZF_LOG_WRITE(lvl, "bfe", __VA_ARGS__)
+#else
+#define LOGGER_DEBUG 0
+#define LOGGER_INFO 0
+#define LOGGER_WARNING 0
+#define LOGGER_ERROR 0
+
+#define logger_log(lvl, ...)
+#endif
 
 #endif
